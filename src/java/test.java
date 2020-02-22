00f749589757ad1e6669f07e3d3673e86949579c
Jack <hyyh0369@163.com>
2020-02-22 22:45:43 +0800

    第二次测试

---
+++ src/java/test.java
@@ -0,0 +1,12 @@
+package java;
+
+/**
+ * @author Jack
+ * @Date: 2020/2/22 22:45
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
