f73060903afba3b2824394a3291959ffc8a5ab69
yangj <846272692@qq.com>
2020-02-22 22:52:08 +0800

    测试-杨晶

---
+++ src/java/NiHao.java
@@ -0,0 +1,15 @@
+package java;
+
+
+
+/**
+ * @ClassName NiHao
+ * @Description TODO
+ * @Author YangJinG
+ * @Date 2020/2/22
+ **/
+public class NiHao {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
