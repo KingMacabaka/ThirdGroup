374452d55ad4f32d75c7123a722cc4226f3d9f4e
Jack <hyyh0369@163.com>
2020-02-22 23:09:42 +0800

    结构分层

---
+++ src/java/part4/test.java
@@ -0,0 +1,12 @@
+package java.part4;
+
+/**
+ * @author Jack
+ * @Date: 2020/2/22 22:45
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
