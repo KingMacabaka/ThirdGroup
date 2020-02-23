31279f419100b700258bc549aba9bea58ce4a975
zyj <1836686674@qq.com>
2020-02-23 18:41:59 +0800

    InterfaceTest

---
+++ src/main/part2/Interface.java
@@ -0,0 +1,13 @@
+package main.part2;
+
+/**
+ * @author Yujie_Zhao
+ * @ClassName Interface
+ * @Description TODO
+ * @Date 2020/2/23  18:34
+ * @Version 1.0
+ **/
+@FunctionalInterface
+public interface Interface {
+    void sayMessage(String message);
+}
---
+++ src/main/part2/test.java
@@ -0,0 +1,30 @@
+package main.part2;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Yujie_Zhao
+ * @ClassName test
+ * @Description 测试
+ * @Date 2020/2/23  0:54
+ * @Version 1.0
+ **/
+
+public class test{
+
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<Student>() {
+            {
+                add(new Student(1001, "路飞", "M"));
+                add(new Student(1005, "明哥", "M"));
+                add(new Student(1000, "娜美", "M"));
+            }
+        };
+        students.sort(new Rank());
+        for (Student student:students) {
+            System.out.println(student.toString());
+        }
+
+    }
+}
